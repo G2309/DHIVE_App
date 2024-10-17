1bc686b757dc5e8d23f91c006c292332f7340b99
gustavo <cru22779@uvg.edu.gt>
2024-10-17 08:27:58 -0600

    New: Color palette

--- app/file_reader.py
+++ app/file_reader.py
@@ -3,5 +3,4 @@ import pandas as pd
 df = pd.read_excel('DATOS_USUARIOS.xlsx')
 df_training = pd.read_excel('Capacitaciones.xlsx')
 df_aux = pd.read_excel('Entradas_Auxiliares.xlsx')
-
-
+df_attendance = pd.read_excel('Registro_entradas.xlsx')
---
+++ interface/app.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode('dark')
+
+app = ctk.CTk()
+app.title("D-HIVE")
+app.attributes('-fullscreen', True)
+
+app.mainloop()

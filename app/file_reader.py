dc9f405bfffe85f6ffe13cd792960b6da8032855
gustavo <cru22779@uvg.edu.gt>
2024-10-17 22:49:05 -0600

    Update: File Reader

--- app/file_reader.py
+++ app/file_reader.py
@@ -1,6 +1,19 @@
+# File reader 
+
 import pandas as pd
 
-df = pd.read_excel('DATOS_USUARIOS.xlsx')
-df_training = pd.read_excel('Capacitaciones.xlsx')
-df_aux = pd.read_excel('Entradas_Auxiliares.xlsx')
-df_attendance = pd.read_excel('Registro_entradas.xlsx')
+# XLSX containing students information (name, lastname, id, etc...)
+def read_user_data(file_path="DATOS_USUARIOS.xlsx"):
+    return pd.read_excel(file_path)
+
+# XLSX containing the name of trainings available
+def read_training_data(file_path="Capacitaciones.xlsx"):
+    return pd.read_excel(file_path)
+
+# XLSX with aux attendance
+def read_aux_data(file_path="Entradas_Auxiliares.xlsx"):
+    return pd.read_excel(file_path)
+
+# XLSX with students attendance
+def read_attendance_data(file_path="Registro_entradas.xlsx"):
+    return pd.read_excel(file_path)
